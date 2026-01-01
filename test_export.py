877e22c92f71117d9ae041e5c238ef492b74062c
chrisschopp <christopher.d.schopp@gmail.com>
2026-01-01 13:32:52 -0500

    Add test of export

--- test_export.py
+++ test_export.py
@@ -1,14 +1,18 @@
 import pytest
+import subprocess
+import shutil
 from anylogic_export import export
 from pathlib import Path
 
+TEST_MODEL = "DistributionCenter"
+EXPERIMENTS = "CustomExperiment", "Simulation"
 
 @pytest.mark.local
 @pytest.mark.parametrize(
     "path_to_model",
     [
-        "d:/repos/anylogic-export/DistributionCenter/DistributionCenter.alpx",
-        "DistributionCenter/DistributionCenter.alpx",
+        f"d:/repos/anylogic-export/{TEST_MODEL}/{TEST_MODEL}.alpx",
+        f"{TEST_MODEL}/{TEST_MODEL}.alpx",
     ],
 )
 def test_good_path(path_to_model: str, tmp_path: Path) -> None:
@@ -35,3 +39,17 @@ def test_good_anylogic_path(anylogic_dir: str) -> None:
 def test_bad_anylogic_path(anylogic_dir: str) -> None:
     with pytest.raises(ValueError):
         export.validated_anylogic_dir(anylogic_dir)
+
+
+@pytest.mark.local
+def test_export() -> None:
+    """Test export after ensuring there are no exported experiment directories.
+    
+    This makes sure it works the first time `export` is run.
+    """
+    for experiment in EXPERIMENTS
+        shutil.rmtree(f"{TEST_MODEL}_{experiment}")
+    subprocess.run(
+        ["python", "./anylogic_export/export.py", "-v", "export"],
+        shell=True,
+    )
